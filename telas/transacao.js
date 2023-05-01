ce2ebd6bd4f2713db483387195cec63e1c83224c
Cicinho <ciciinhocaramellotop@gmail.com>
2023-05-01 18:11:59 -0300

    :)

--- telas/transacao.js
+++ telas/transacao.js
@@ -1,8 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground, Image} from 'react-native';
 import * as Permissions from 'expo-permissions';
 import {BarCodeScanner} from 'expo-barcode-scanner';
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import { TextInput, TouchableOpacity } from 'react-native-gesture-handler';
+
+const bg = require('../assets/background2.png');
+const logo = require('../assets/appIcon.png');
+const appName = require("../assets/appName.png");
+
+
 
 class Transacao extends React.Component {
     constructor(){
@@ -11,61 +17,153 @@ class Transacao extends React.Component {
             appState: 'normal',
             permissao: null,
             scanned: false,
-            dado: "",
+            bookId: '',
+            studentId: '',
         }
     }
 
-    getCameraPermissions = async() => {
+    getCameraPermissions = async(input) => {
         const {status} = await Permissions.askAsync(Permissions.CAMERA);
         this.setState({
             permissao: status === 'granted',
-            appState: 'scan',
+            appState: input,
         })
     }
 
     handleBarCodeScanned = async({type, data}) => {
-        this.setState({
-            dado: data,
-            scanned: true,
-            appState: 'normal',
-        })
+        const {appState} = this.state
+        if(appState === 'bookId'){
+            this.setState({
+                bookId: data,
+                scanned: true,
+                appState: 'normal',
+            })
+        }
+        if(appState === 'studentId'){
+            this.setState({
+                studentId: data,
+                scanned: true,
+                appState: 'normal',
+            })
+        }
     }
 
     render(){
 
         const {appState,permissao,scanned,dado} = this.state;
-        if(appState === 'scan'){
+        if(appState !== 'normal'){
             return(<BarCodeScanner onBarCodeScanned={scanned? undefined : this.handleBarCodeScanned()}/>)
         }
 
 
         return (
-            <View style = {styles.site}>
-                <TouchableOpacity onPress = {() => {
-                    this.getCameraPermissions()
-                }}>
-                    <Text>
-                        Digitalizar QR Code 
-                    </Text>
+            <View style = {styles.container}>
+                <ImageBackground source = {bg} style = {styles.bgImage}>
+                    <View style = {styles.upperContainer}>
+                        <Image source = {logo} style = {styles.appIcon} />
+                        <Image source = {appName} style = {styles.appName} />
+                    </View>
+                    <View style = {styles.lowerContainer}>
+                        <View style = {styles.textInputContainer}>
+                            <TextInput style = {styles.textInput}
+                            placeHolder = {'ID do livro'}
+                            placeHolderTextColor = {'#fff'}
+                            value = {bookId} />
+                            <TouchableOpacity onPress = {() => {
+                            this.getCameraPermissions('bookId')
+                            }}>
+                                <Text>Digitalizar</Text>
+                            </TouchableOpacity>
+                        </View>
+                        <View style = {styles.textInputContainer}>
+                            <TextInput style = {styles.textInput}
+                            placeHolder = {'ID do aluno'}
+                            placeHolderTextColor = {'#fff'}
+                            value = {studentId} />
+                            <TouchableOpacity onPress = {() => {
+                            this.getCameraPermissions('studentId')
+                            }}>
+                                <Text>Digitalizar</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                    <TouchableOpacity onPress = {() => {
+                        this.getCameraPermissions()
+                    }}>
+                        <Text>
+                            Digitalizar QR Code 
+                        </Text>
 
 
-                </TouchableOpacity>
+                    </TouchableOpacity>
+                </ImageBackground> 
             </View>
         )
     }
 }
 
 const styles = StyleSheet.create({
-    site:{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#5653d4',        
+    container: {
+      flex: 1,
+      backgroundColor: "#FFFFFF"
     },
-    text: {
-        color: '#fff',
-        fontSize: 30,
+    bgImage: {
+      flex: 1,
+      resizeMode: "cover",
+      justifyContent: "center"
+    },
+    upperContainer: {
+      flex: 0.5,
+      justifyContent: "center",
+      alignItems: "center"
+    },
+    appIcon: {
+      width: 200,
+      height: 200,
+      resizeMode: "contain",
+      marginTop: 80
+    },
+    appName: {
+      width: 180,
+      resizeMode: "contain"
+    },
+    lowerContainer: {
+      flex: 0.5,
+      alignItems: "center"
+    },
+    textinputContainer: {
+      borderWidth: 2,
+      borderRadius: 10,
+      flexDirection: "row",
+      backgroundColor: "#9DFD24",
+      borderColor: "#FFFFFF"
+    },
+    textinput: {
+      width: "57%",
+      height: 50,
+      padding: 10,
+      borderColor: "#FFFFFF",
+      borderRadius: 10,
+      borderWidth: 3,
+      fontSize: 18,
+      backgroundColor: "#5653D4",
+      fontFamily: "Rajdhani_600SemiBold",
+      color: "#FFFFFF"
+    },
+    scanbutton: {
+      width: 100,
+      height: 50,
+      backgroundColor: "#9DFD24",
+      borderTopRightRadius: 10,
+      borderBottomRightRadius: 10,
+      justifyContent: "center",
+      alignItems: "center"
+    },
+    scanbuttonText: {
+      fontSize: 20,
+      color: "#0A0101",
+      fontFamily: "Rajdhani_600SemiBold"
     }
-})
+   });   
 
 export default Transacao
